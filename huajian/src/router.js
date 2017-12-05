79f10ffcc9e3794dd7a2a23657d646055d363124
wanghuajian620 <154659041@qq.com>
2017-12-05 18:47:31 +0800

    布局，卡片

--- huajian/src/router.js
+++ huajian/src/router.js
@@ -1,31 +1,31 @@
-// import React from 'react';
-// import { Router, Route, Switch } from 'dva/router';
-// // import Apple from './routes/Apple';
-// import BasicLayout from './layouts/Basiclayouts';
-// import UserLayout from './layouts/Userlayout';
-
-// function RouterConfig({ history }) {
-//   return (
-//     <Router history={history}>
-//       <Switch>
-//         <Route path="/user" exact component={UserLayout} />
-//         <Route path="/" exact component={BasicLayout} />
-//       </Switch>
-//     </Router>
-//   );
-// }
-
-// export default RouterConfig;
-// .src/router.js
 import React from 'react';
-import { Router, Route } from 'dva/router';
-import Users from './routes/Users';
+import { Router, Route, Switch } from 'dva/router';
+// import Apple from './routes/Apple';
+import BasicLayout from './layouts/Basiclayouts';
+import UserLayout from './layouts/Userlayout';
 
-export default function ({ history }) {
+function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Route path="/users" component={Users} />
+      <Switch>
+        <Route path="/user" exact component={UserLayout} />
+        <Route path="/" exact component={BasicLayout} />
+      </Switch>
     </Router>
   );
 }
+
+export default RouterConfig;
+// // .src/router.js
+// import React from 'react';
+// import { Router, Route } from 'dva/router';
+// import Users from './routes/Users';
+
+// export default function ({ history }) {
+//   return (
+//     <Router history={history}>
+//       <Route path="/users" component={Users} />
+//     </Router>
+//   );
+// }
 
