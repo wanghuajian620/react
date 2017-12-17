6d6dd01680a500cf9c44a0747c1408e1bfec6abb
wanghuajian620 <154659041@qq.com>
2017-12-17 21:35:38 +0800

    新增文章，图片

--- ui/src/routes/Menu.js
+++ ui/src/routes/Menu.js
@@ -53,9 +53,50 @@ class Bar extends React.PureComponent {
                 alt="图片"
               />
             </div>
-            <div>
+            <div className={styles.global}>
               <Row type="flex" justify="space-around" align="top">
-                <Col span={12}><DemoBox value={200}>col-4</DemoBox></Col>
+                <Col span={12} className={styles.text}>
+                  <DemoBox value={200}>
+                    <p style={{ marginBottom: '1.25em', fontSize: '1em' }}>
+                    At Maaemo, our focus is on a complete experience.
+                    In doing so, we want to highlight the relationship between the raw produce,
+                    and the expression in the cuisine.
+                    </p>
+                    <p style={{ marginBottom: '1.25em', fontSize: '1em' }}>
+                    We have chosen to serve a distinct and considered set menu to all guests.
+                    </p>
+                    <p style={{ marginBottom: '1.25em', fontSize: '1em' }}>
+                    The drink menus have been carefully curated to add an extra dimension to
+                    each of the courses.This way we can ensure our guests have a unique and
+                    precise experience on every occasion
+                    </p>
+                    <table className={styles.table}>
+                      <tr>
+                        <td style={{ textAlign: 'left' }}>The Seasonal Menu</td>
+                        <td style={{ marginright: 10 }}>kr</td>
+                        <td>2 800</td>
+                      </tr>
+                      <tr>
+                        <td style={{ textAlign: 'left' }}>Wine Pairings</td>
+                        <td>kr</td>
+                        <td>1 800</td>
+                      </tr>
+                      <tr>
+                        <td style={{ textAlign: 'left' }}>Juice Pairings</td>
+                        <td>kr</td>
+                        <td>9 50</td>
+                      </tr>
+                    </table>
+                  </DemoBox>
+                </Col>
+                <Col span={12} className={styles.picture}>
+                  <DemoBox value={200}>
+                    <img
+                      className={styles.inpicture}
+                      src="https://d27shkkua6xyjc.cloudfront.net/images/01.Menu/_1600x1600_fit_center-center/Menu_01_1200px.jpg?mtime=20141112152700" alt=""
+                    />
+                  </DemoBox>
+                </Col>
               </Row>
             </div>
           </Content>
