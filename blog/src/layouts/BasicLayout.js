d9f56fa050aa236b4f09fc6dd9e09e38af580742
wanghuajian620 <154659041@qq.com>
2017-12-22 21:04:40 +0800

    简单界面

--- blog/src/layouts/BasicLayout.js
+++ blog/src/layouts/BasicLayout.js
@@ -9,22 +9,28 @@ class BasicLayout extends React.PureComponent {
   render() {
     return (
       <div className={styles.body}>
-        <Layout>
+        <Layout className={styles.site}>
           <Content>
-            <div className={styles.allphoto}>
-              <img
-                className={styles.firstphoto}
-                src="https://www.robmills.com.au/files/landing/_largeImage/rma_home_land_01.jpg" alt="" />
-              <img
-                className={styles.firstphoto}
-                src="https://www.robmills.com.au/files/landing/_largeImage/rma_home_architecture_01.jpg" alt="" />
-              <img
-                className={styles.firstphoto}
-                src="https://www.robmills.com.au/files/landing/_largeImage/rma_stuartstreet_06.jpg" alt="" />
-              <img
-                className={styles.firstphoto}
-                src="https://www.robmills.com.au/files/landing/_largeImage/Rob-Mills-Rural26839.jpg" alt="" />
-            </div>
+            <img
+              className={styles.photo}
+              src="https://www.robmills.com.au/files/landing/_largeImage/rma_home_land_01.jpg"
+              alt=""
+            />
+            <img
+              className={styles.photo}
+              src="https://www.robmills.com.au/files/landing/_largeImage/rma_home_architecture_01.jpg"
+              alt=""
+            />
+            <img
+              className={styles.photo}
+              src="https://www.robmills.com.au/files/landing/_largeImage/rma_stuartstreet_06.jpg"
+              alt=""
+            />
+            <img
+              className={styles.photo}
+              src="https://www.robmills.com.au/files/landing/_largeImage/Rob-Mills-Rural26839.jpg"
+              alt=""
+            />
           </Content>
           <Footer className={styles.footer}>
             Architects & Interior Designers based in Melbourne, Sydney and Perth
