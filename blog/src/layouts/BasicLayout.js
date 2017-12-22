1e44fa350a03a9546d90b2e31f3d83d74e98c9da
wanghuajian620 <154659041@qq.com>
2017-12-22 18:10:28 +0800

    加入content footer

--- blog/src/layouts/BasicLayout.js
+++ blog/src/layouts/BasicLayout.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Layout } from 'antd';
+import styles from './BasicLayout.less';
+
+const { Content, Footer } = Layout;
 
 
 class BasicLayout extends React.PureComponent {
   render() {
     return (
-      <div>明日 开工！！！</div>
+      <div className={styles.body}>
+        <Layout>
+          <Content>
+            <div className={styles.allphoto}>
+              <img
+                className={styles.firstphoto}
+                src="https://www.robmills.com.au/files/landing/_largeImage/rma_home_land_01.jpg" alt="" />
+              <img
+                className={styles.firstphoto}
+                src="https://www.robmills.com.au/files/landing/_largeImage/rma_home_architecture_01.jpg" alt="" />
+              <img
+                className={styles.firstphoto}
+                src="https://www.robmills.com.au/files/landing/_largeImage/rma_stuartstreet_06.jpg" alt="" />
+              <img
+                className={styles.firstphoto}
+                src="https://www.robmills.com.au/files/landing/_largeImage/Rob-Mills-Rural26839.jpg" alt="" />
+            </div>
+          </Content>
+          <Footer className={styles.footer}>
+            Architects & Interior Designers based in Melbourne, Sydney and Perth
+          </Footer>
+        </Layout>
+      </div>
     );
   }
 }
