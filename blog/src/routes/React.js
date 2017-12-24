73f78e94e53c5bb4cd058c727242f3182155b85b
wanghuajian620 <154659041@qq.com>
2017-12-24 11:11:25 +0800

    基本布局完成

--- blog/src/routes/React.js
+++ blog/src/routes/React.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Card, Pagination, BackTop } from 'antd';
 import styles from './React.less';
 
 const { Header, Content, Footer } = Layout;
 
 
 class ReaLayout extends React.Component {
+  state = {
+    current: 3,
+  }
+  onChange = (page) => {
+    this.setState({
+      current: page,
+    });
+  }
   render() {
     return (
       <div>
@@ -30,7 +38,31 @@ class ReaLayout extends React.Component {
               </Menu.Item>
             </Menu>
           </Header>
-          <Content style={{ minHeight: '100vh' }}>Content</Content>
+          <Content className={styles.content}>
+            <Card title="Card title" className={styles.card}>
+              圣诞了，考研又进入倒计时了。
+            </Card>
+            <Card title="Card title" className={styles.card}>
+              圣诞了，考研又进入倒计时了。
+            </Card>
+            <Card title="Card title" className={styles.card}>
+              圣诞了，考研又进入倒计时了。
+            </Card>
+            <Card title="Card title" className={styles.card}>
+              圣诞了，考研又进入倒计时了。
+            </Card>
+            <Pagination
+              current={this.state.current} onChange={this.onChange} total={50}
+              className={styles.page}
+            />
+            <BackTop />
+            <strong
+              className={styles.texthidden}
+              style={{ color: 'rgba(50, 50, 50, 0.6)' }}
+            >
+              回到顶部
+            </strong>
+          </Content>
           <Footer>sfsf</Footer>
         </Layout>
       </div>
