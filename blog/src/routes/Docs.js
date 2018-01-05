b7992139f73f96cfa642e160a67599e1ebb04f01
wanghuajian620 <154659041@qq.com>
2018-01-05 18:12:31 +0800

    文章详情页初步完成

--- blog/src/routes/Docs.js
+++ blog/src/routes/Docs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Menu, Card, Icon, BackTop } from 'antd';
 import styles from './Docs.less';
 
 const { Header, Content, Footer } = Layout;
@@ -8,15 +8,51 @@ class Doc extends React.Component {
   render() {
     return (
       <div>
-        <Header className={styles.header}>
-          2018
-        </Header>
-        <Content className={styles.content}>
-          新年新气象
-        </Content>
-        <Footer className={styles.footer}>
-          那可厉害了
-        </Footer>
+        <Layout>
+          <Header className={styles.header}>
+            <h3 className={styles.text}>Christmas blog</h3>
+            <Menu
+              mode="horizontal"
+              className={styles.menu}
+            >
+              <Menu.Item key="1">Back</Menu.Item>
+              <Menu.Item key="2">归档</Menu.Item>
+              <Menu.Item key="3">关于</Menu.Item>
+            </Menu>
+          </Header>
+          <Content className={styles.content}>
+            <Card title="Card title" className={styles.card}>
+              <p>Card content</p>
+            </Card>
+            <div className={styles.backtop}>
+              <BackTop visibilityHeight={0} />
+            </div>
+          </Content>
+          <Footer>
+            <div className={styles.footer}>
+              <a
+                className={styles.bottom}
+                href="https://github.com/wanghuajian620"
+              >
+                GitHub
+              </a>
+              <a
+                className={styles.bottom}
+                href="http://gmail.com/wanghuajian620"
+              >
+              Gmail
+              </a>
+              <a
+                href="http://gmail.com/wanghuajian620"
+              >
+              Blog
+              </a>
+              <div>
+                Copyright <Icon type="copyright" /> 2017 个人博客圣诞夜出品
+              </div>
+            </div>
+          </Footer>
+        </Layout>
       </div>
     );
   }
