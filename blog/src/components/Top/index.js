aa2c9e3c8f8ffa079a4f06feff18e748d6bf4d95
wanghuajian620 <154659041@qq.com>
2018-01-11 15:03:22 +0800

    把文章详情页的头部改写为组件Top。

---
+++ blog/src/components/Top/index.js
@@ -0,0 +1,34 @@
+/*
+2018-01-11 Wang huajian
+*/
+
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { Link } from 'dva/router';
+import styles from './index.less';
+
+const { Header } = Layout;
+
+function Top(props) {
+  console.log(props); // eslint-disable-line
+  return (
+    <Header className={styles.header}>
+      <h3 className={styles.text}>Christmas blog</h3>
+      <Menu
+        mode="horizontal"
+        className={styles.menu}
+      >
+        <Menu.Item key="1">
+          <Link to="/">
+          Back
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="2">归档</Menu.Item>
+        <Menu.Item key="3">关于</Menu.Item>
+      </Menu>
+    </Header>
+  );
+}
+
+export default Top;
+
--- blog/src/routes/Docs.js
+++ blog/src/routes/Docs.js
@@ -1,33 +1,24 @@
 import React from 'react';
-import { Layout, Menu, Card, BackTop } from 'antd';
+import { Layout, Card, BackTop } from 'antd';
+import Top from '../components/Top';
 import Footer from '../components/Footer';
 import styles from './Docs.less';
 
-const { Header, Content } = Layout;
-const ReactMarkdown = require('react-markdown');
+const { Content } = Layout;
+// const ReactMarkdown = require('react-markdown');
 
 class Doc extends React.Component {
   render() {
     return (
       <div>
         <Layout>
-          <Header className={styles.header}>
-            <h3 className={styles.text}>Christmas blog</h3>
-            <Menu
-              mode="horizontal"
-              className={styles.menu}
-            >
-              <Menu.Item key="1">Back</Menu.Item>
-              <Menu.Item key="2">归档</Menu.Item>
-              <Menu.Item key="3">关于</Menu.Item>
-            </Menu>
-          </Header>
+          <Top />
           <Content className={styles.background}>
             <Card title="Card title" className={styles.card}>
               <p>Card content</p>
             </Card>
-            <BackTop visibilityHeight={0} />
           </Content>
+          <BackTop visibilityHeight={200} />
           <Footer />
         </Layout>
       </div>
