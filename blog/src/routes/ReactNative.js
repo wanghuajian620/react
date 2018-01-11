d4d84ce11e246376f425115ae80decbdce918938
wanghuajian620 <154659041@qq.com>
2018-01-11 14:04:13 +0800

    ReactNative, Angular关联model，并实现父子组件之间的传递属性

--- blog/src/routes/ReactNative.js
+++ blog/src/routes/ReactNative.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Layout, Pagination } from 'antd';
+import { connect } from 'dva';
+import { Link } from 'dva/router';
 import Artical from '../components/Artical';
 import Footer from '../components/Footer';
 import styles from './React.less';
+
+const { Content } = Layout;
 
 class Reactnative extends React.Component {
   state = {
@@ -19,10 +23,26 @@ class Reactnative extends React.Component {
     });
   }
   render() {
+    const { reactnative } = this.props;
     return (
       <div>
         <Layout className={styles.background}>
-          <Artical />
+          <Content className={styles.content}>
+            <div className={styles.logo}>
+              <Link to="/">
+                <img src="http://www.logobook.com/wp-content/uploads/2017/03/MorseLife_logo.svg" alt="" />
+              </Link>
+            </div>
+            {
+              reactnative.content.map(item => <Artical
+                title={item.title}
+                key={item.key}
+                date={item.date}
+                tag={item.tag}
+                essay={item.essay}
+              />)
+            }
+          </Content>
           <Pagination
             current={this.state.current} onChange={this.onChange} total={50}
             className={styles.page}
@@ -34,4 +54,6 @@ class Reactnative extends React.Component {
   }
 }
 
-export default Reactnative;
+export default connect(state => ({
+  reactnative: state.reactnative,
+}))(Reactnative);
