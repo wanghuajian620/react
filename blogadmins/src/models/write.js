9a0dc1afe321ddcdc60bb6089162b833ac3a42b4
wanghuajian620 <154659041@qq.com>
2018-01-14 17:06:30 +0800

    代码规范

--- blogadmins/src/models/write.js
+++ blogadmins/src/models/write.js
@@ -2,7 +2,6 @@
  * 2018-01-14 Wang huajian
 */
 
-// import { routerRedux } from 'dva/router';
 import { WriteEssay } from '../services/write';
 
 export default {
@@ -12,14 +11,12 @@ export default {
 
   effects: {
     * writeessay({ payload }, { call }) {
-      console.log(payload, 'yyyyyyyyy');
       const params = {
         title: payload.writetitle,
         article: payload.writearticle,
       };
-      console.log(params, 'bbbbbb');
       const result = yield call(WriteEssay, params);
-      console.log(result, 'mmmmm');
+      console.log(result, 'mmmmm'); // eslint-disable-line
       if (result.status === 0) {
         // yield put(routerRedux.push('/main'));
       }
