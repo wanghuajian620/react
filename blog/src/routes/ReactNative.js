52dabbfb190d60f4a9886a633dc47830f41d99af
wanghuajian620 <154659041@qq.com>
2018-01-14 17:38:32 +0800

    从文章到文章详情页已 全部改为组件切换

--- blog/src/routes/ReactNative.js
+++ blog/src/routes/ReactNative.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Layout, Pagination, BackTop } from 'antd';
 import { connect } from 'dva';
+import { Switch, Route } from 'react-router-dom';
 import Logo from '../components/Logo';
 import Artical from '../components/Artical';
+import Articledetail from '../components/Articledetail';
 import Footer from '../components/Footer';
 import styles from './React.less';
 
@@ -28,20 +30,34 @@ class Reactnative extends React.Component {
       <div>
         <Layout className={styles.background}>
           <Content className={styles.content}>
-            <Logo />
-            {
-              reactnative.content.map(item => <Artical
-                title={item.title}
-                key={item.key}
-                date={item.date}
-                tag={item.tag}
-                essay={item.essay}
-              />)
-            }
-            <Pagination
-              current={this.state.current} onChange={this.onChange} total={50}
-              className={styles.page}
-            />
+            <Switch>
+              <Route
+                exact
+                path="/reactnative"
+                render={() => (
+                  <div>
+                    <Logo />
+                    {
+                      reactnative.content.map(item => <Artical
+                        title={item.title}
+                        key={item.key}
+                        date={item.date}
+                        tag={item.tag}
+                        essay={item.essay}
+                      />)
+                    }
+                    <Pagination
+                      current={this.state.current} onChange={this.onChange} total={50}
+                      className={styles.page}
+                    />
+                  </div>
+                )}
+              />
+              <Route
+                path="/reactnative/reactnative-essay"
+                render={() => <Articledetail />}
+              />
+            </Switch>
           </Content>
           <BackTop visibilityHeight={200} />
           <Footer />
