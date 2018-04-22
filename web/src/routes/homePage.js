8e8b9276d543fcbae2adc68acad88070eadfe075
wanghuajian620 <154659041@qq.com>
2018-04-22 19:37:06 +0800

    import Layout

--- web/src/routes/homePage.js
+++ web/src/routes/homePage.js
@@ -6,15 +6,17 @@
 import React from 'react';
 import { Layout } from 'antd';
 
+import styles from '../routes/home.css';
+
 const { Header, Footer, Content } = Layout;
 class HomePage extends React.Component {
   render() {
     return (
       <div>
         <Layout>
-          <Header>头部</Header>
-          <Content>内容</Content>
-          <Footer>底部</Footer>
+          <Header className={styles.header}>头部</Header>
+          <Content className={styles.content}>内容</Content>
+          <Footer className={styles.footer}>底部</Footer>
         </Layout>
       </div>
     );
