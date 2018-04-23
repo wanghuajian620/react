f2f8f03b6a2bcd0dfee8dcb9f621a8ac6bc03e0c
wanghuajian620 <154659041@qq.com>
2018-04-23 18:52:45 +0800

    add menu

--- web/src/routes/homePage.js
+++ web/src/routes/homePage.js
@@ -4,17 +4,44 @@
  */
 
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 
 import styles from '../routes/home.css';
 
 const { Header, Footer, Content } = Layout;
+
 class HomePage extends React.Component {
+  state = {
+    current: 'home',
+  }
+  handleClick = (e) => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  }
   render() {
     return (
       <div>
         <Layout>
-          <Header className={styles.header}>头部</Header>
+          <Header className={styles.header}>
+            <Menu
+            onClick={this.handleClick}
+            selectedKeys={[this.state.current]}
+            mode="horizontal"
+            className={styles.menu}
+          >
+              <Menu.Item key="home">
+              <Icon type="home" />首页
+              </Menu.Item>
+              <Menu.Item key="download">
+                <Icon type="cloud-download" />下载文件
+              </Menu.Item>
+              <Menu.Item key="user">
+                <Icon type="user" />我的
+              </Menu.Item>
+            </Menu>
+          </Header>
           <Content className={styles.content}>内容</Content>
           <Footer className={styles.footer}>底部</Footer>
         </Layout>
