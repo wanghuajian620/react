315ed5a23c7565885d3e0e8e0c9a4da2b0d7981b
wanghuajian620 <154659041@qq.com>
2019-04-01 11:24:43 +0800

    修改下之前的认知错误

--- blogadmins/src/utils/request.js
+++ blogadmins/src/utils/request.js
@@ -5,7 +5,7 @@ import HttpStatus from 'http-status-codes';
 
 axios.defaults.baseURL = 'http://192.168.0.109:8080';
 axios.defaults.timeout = 10000;
-axios.defaults.withCredentials = true; // 解决的是session的问题，不写一直报401未登录
+axios.defaults.withCredentials = true; // 解决的是跨域的问题
 
 const fetch = (options) => {
   const {
